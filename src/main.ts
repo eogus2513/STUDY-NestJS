8913ab1cca68854ad44b7267a3a2d62587ab2b3d
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-03-04 09:12:37 +0900

    ⚡️ :: Validation 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ import { httpExceptionFilter } from './common/exceptions/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 없은 속성은 무조건 거른다.
+      forbidNonWhitelisted: true, //전달하는 요청 값 중에 정의 되지 않은 값이 있으면 Error를 발생
+      transform: true, //javascript 객체 -> DTO로 변환
+      disableErrorMessages: false, //Error가 발생 했을 때 Error Message를 표시 여부 설정
+    }),
+  );
   app.useGlobalFilters(new httpExceptionFilter());
   app.enableCors({
     origin: true,
