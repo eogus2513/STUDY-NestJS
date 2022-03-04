26c959481afeb21cb1620c2ee26ae788ff444c98
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-03-04 10:23:21 +0900

    ⚡️ :: socket.io -> SOCKET으로 변경

--- src/event/event.gateway.ts
+++ src/event/event.gateway.ts
@@ -26,7 +26,7 @@ export class EventGateway
 
   @WebSocketServer() server: Server;
 
-  private logger = new Logger('socket.io');
+  private logger = new Logger('SOCKET');
 
   @SubscribeMessage('events')
   handleEvent(@MessageBody() data: string): string {
