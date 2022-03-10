03486eee3ac61a4ebd8745f89b3726e123040437
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-03-10 21:11:58 +0900

    ⚡️ :: redis 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,23 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entity/user.entity';
 import { AuthModule } from '../auth/auth.module';
 import { JwtStrategy } from '../auth/jwt/jwt.strategy';
+import { UserRepository } from './entity/repository/user.repository';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: +process.env.REDIS_PORT,
+      ttl: 0,
+    }),
+    AuthModule,
+  ],
   controllers: [UserController],
   providers: [UserService, JwtStrategy],
   exports: [UserService],
